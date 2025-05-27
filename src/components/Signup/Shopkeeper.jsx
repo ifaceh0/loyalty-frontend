587037aa8d45a189832f7116cadb1d391e5d2303
debaaaa01 <debashishdas0108@gmail.com>
2025-05-28 01:15:26 +0530

    change in shopkeeper.jsx

--- src/components/Signup/Shopkeeper.jsx
+++ src/components/Signup/Shopkeeper.jsx
@@ -102,7 +102,7 @@ function Shopkeeper() {
           type="tel"
           name="phoneNumber"
           placeholder="Phone Number"
-          value={formData.Number}//
+          value={formData.phoneNumber}//
           onChange={handleChange}
           className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-200"
           required
