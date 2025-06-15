b831fa95449c5dd86317060c79a0e8229454b02a
debaaaa01 <debashishdas0108@gmail.com>
2025-06-15 18:51:57 +0530

    demo images, few major chnages , graphs, dashboard split

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div>
-     <h1 className="text-4xl text-center mt-60">Home Page</h1>
+    <div className="relative w-full h-screen overflow-hidden">
+      <img
+        src="images/loyalty.jpg" 
+        alt="Home Banner"
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      />
+      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40">
+        <h1 className="text-white text-3xl font-bold">Get your points</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
