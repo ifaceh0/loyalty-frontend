1c4fa92664be4c4fde5ec88d05d7b2e9c252b9a1
Aryaa-7 <aryanojha07@gmail.com>
2025-06-19 16:59:02 +0530

    Error resolved

--- src/components/ShopList/QRModal.jsx
+++ src/components/ShopList/QRModal.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import React, { useEffect, useState } from 'react';
+
+import { useEffect, useState } from 'react';
 
 export default function QRModal({ shop, isOpen, onClose }) {
   const [qrData, setQrData] = useState(null);
--- src/components/Signup/User.jsx
+++ src/components/Signup/User.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import Confetti from "react-confetti";
 
 function User() {
